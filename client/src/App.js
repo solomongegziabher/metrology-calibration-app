3278bdf1aa03c448c0836f539b50ee2ec5ef96ea
Solomon <113886983+solomongegziabher@users.noreply.github.com>
2023-11-24 13:50:10 +0300

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,1 +1,14 @@
+// client/src/App.js
+import React from 'react';
+import CalibrationForm from './components/CalibrationForm';
 
+function App() {
+  return (
+    <div className="App">
+      <h1>Metrology and Calibration </h1>
+      <CalibrationForm />
+    </div>
+  );
+}
+
+export default App;
