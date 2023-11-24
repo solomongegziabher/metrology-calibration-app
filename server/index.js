0819b323238db5fc5836837b88c67457138172f3
Solomon <113886983+solomongegziabher@users.noreply.github.com>
2023-11-24 13:51:46 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,1 +1,21 @@
+// server/index.js
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 5000;
+
+app.use(bodyParser.json());
+
+app.post('/submit_calibration', (req, res) => {
+  const { equipment, calibrationDate } = req.body;
+
+  // Process and store calibration data in the database (not implemented in this example)
+
+  res.json({ success: true, message: `Calibration submitted for ${equipment} on ${calibrationDate}.` });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 
